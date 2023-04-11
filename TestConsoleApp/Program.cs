e73eaa2590fc36a00c84d75924fa382f5463522f
Gowtham Kolagani <Gowtham.Kolagani@jll.com>
2023-04-11 20:11:19 +0530

    update

---
+++ TestConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Data.SqlClient;
+
+Console.WriteLine("Hello, World!");
+var connString = Environment.GetEnvironmentVariable("dbConnectionString");
+
+Console.WriteLine(connString);
+
+var sql = "select * from employee";
+
+using (var connection = new SqlConnection("Data Source=INBLR3LB2MN9K3;Initial Catalog=Test;Integrated Security=True;TrustServerCertificate=True"))
+{
+    connection.Open();
+    using (var command = new SqlCommand(sql, connection))
+    {
+        using (var reader = command.ExecuteReader())
+        {
+           
+        }
+    }
+}
+
+
+Console.ReadLine();
---
+++ testlib/employee.cs
@@ -0,0 +1,7 @@
+﻿namespace testlib
+{
+    public class employee
+    {
+
+    }
+}
